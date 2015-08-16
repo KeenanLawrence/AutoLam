78e2aa6b7640f73e25adddce9ff3fbbc07955770
KeenanLawrence <lwrkee002@myuct.ac.za>
2015-08-16 18:23:01 +0200

    Rectified the alphaConvert() method, one test case is failing

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,15 @@
 import java.util.*;
 
 public class Calculator {
-	//Expression expression;
-	String expression;
-	Question question;
-	boolean redundantConversion;
-	List <String> history; 
+	
 	List <String> func; 
-	List <String> arg; 
+	List <String> bound;
+	
 	String tempExpr;
 	
 	public Calculator(String e){
-		expression = e;
 		func = new ArrayList <String> ();
-		arg = new ArrayList <String> ();
+		bound = new ArrayList <String> ();
 		tempExpr = "";
 	}
 	
@@ -32,14 +28,17 @@ public class Calculator {
 		}
 	}
 	
+	//Method to find function names and things that are bound to the function
 	public void findBinding (String e){
 		int parenthCount = 0;
 		int index = 0;
 		
+		//Remove all outermost brackets
 		while (!stripBrackets(e).equals("Halt")){
 			e = stripBrackets(e);
 		}
 		
+		//Loop through the entire string and determine what to do 
 		for (int i = 0; i < e.length(); i++){
 			if (e.charAt(i) == '/'){
 				func.add(Character.toString(e.charAt(i+1)));
@@ -58,50 +57,64 @@ public class Calculator {
 						parenthCount--;
 					}
 				}
-				arg.add(e.substring(i+1, index));
+				bound.add(e.substring(i+2, index));
 				}
 				else{
-					arg.add(e.substring(i+1));
+					if (e.charAt(i+1) != '/'){
+						bound.add(e.substring(i+1));
+					}
 					
 				}
 			}
 		}
 	}
+	
 	public String alphaConvert (String expr, String target, String choice){
-		boolean random = false;
-		tempExpr = "";
+		
 		findBinding(expr);
 		
+		//If the choice of substitution is already in the expression,
+		//or the thing to substitute does not exist, return an error.
 		if (expr.contains(choice) || !expr.contains(target)){
 			tempExpr = "Illegal substitution";
 			return tempExpr;
 		}
 		
 		//For debugging purposes
-		/*
 		for (int i = 0; i < func.size(); i++){
 			System.out.println("Func");
 			System.out.println(func.get(i));
 		}
-		for (int i = 0; i < arg.size(); i++){
-			System.out.println("Arg");
-			System.out.println(arg.get(i));
+		for (int i = 0; i < bound.size(); i++){
+			System.out.println("Bound");
+			System.out.println(bound.get(i));
 		}
-		*/
 		
+		//Checks if the variable to replace is a function
 		for (int i = 0; i < func.size(); i++){
-			
-			if (func.get(i).contains(target) && arg.get(i).contains(target)){
-				tempExpr = expr.replaceFirst(target, func.get(i).replace(target, choice));
-				tempExpr = tempExpr.replaceFirst(target, choice);
-				random = true;
-				
+			if (func.get(i).contains(target)){
+				//If it is, check if there's a bounded variable of the same name
+				for (int k = 0; k < bound.size(); k++){
+					if (bound.get(k).contains(target)){
+						//Replace the first occurrence of the function name
+						tempExpr = expr.replaceFirst(target, func.get(i).replace(target, choice));
+						System.out.println(tempExpr);
+						//Replace the bounded variable name
+						tempExpr = tempExpr.replaceFirst(target, bound.get(k).replace(target, choice));
+						break;
+					}
+				}
 			}
-			if (!func.get(i).contains(target) && arg.get(i).contains(target) && random){
-				tempExpr = tempExpr.replace(target, choice);
+			
+			if (func.get(i).contains(target)){
+				//Check if there's a bounded variable of the same name. If not, just change the function name
+				for (int k = 0; k < bound.size(); k++){
+					if (!bound.get(k).contains(target)){
+						tempExpr = expr.replaceFirst(target, choice);
+						break;
+					}	
+				}
 			}
-			
-			
 		}
 		return tempExpr; 
 	}
